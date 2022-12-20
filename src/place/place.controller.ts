f77280c161404b81570e4570337b324b64baf8f3
accela673 <sardar.kasmaliev@alatoo.edu.kg>
2022-12-21 00:47:59 +0600

    fixed bugs

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -2,25 +2,19 @@ import { UpdatePlace } from './dto/update.place.dto';
 import { CreatePlace } from './dto/create.place.dto';
 import { PlaceService } from './place.service';
 import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put, ValidationPipe } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
-import { Req, UploadedFile, UseGuards, UseInterceptors, UsePipes } from '@nestjs/common/decorators';
+import {  ApiBody, ApiConsumes, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Req, UploadedFile,  UseInterceptors, UsePipes } from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guards';
 
 const arr = ['email@gmail.com']
 
-const validate = (email: string) => {
-  if(!arr.includes(email)) {throw new BadRequestException("You're not permitted to this endpoint")}  
-}
 
 @ApiTags("Place")
 @Controller('place')
 export class PlaceController {
     constructor (private readonly placeService: PlaceService){}
 
-    @ApiBearerAuth()
-    @ApiUnauthorizedResponse()
-    @UseGuards(JwtAuthGuard)
+
     @ApiOperation({description: "This endpoint to get one place from shop"})
     @Get('/get/:id')
     async getOne(@Param('id') id: string){
@@ -28,18 +22,14 @@ export class PlaceController {
     }
 
 
-    @ApiBearerAuth()
-    @ApiUnauthorizedResponse()
-    @UseGuards(JwtAuthGuard)
+
     @ApiOperation({description: "This endpoint to get all place from shop"})
     @Get('/getall')
     async getAll(){
         return await this.placeService.getPlaces()
     }
 
-    @ApiBearerAuth()
-    @ApiUnauthorizedResponse()
-    @UseGuards(JwtAuthGuard)
+
     @ApiOperation({description: "This endpoint to adding place to shop"})
     @ApiConsumes('multipart/form-data')
     @ApiBody({
@@ -58,7 +48,8 @@ export class PlaceController {
     @UseInterceptors(FileInterceptor('place_image'))
     @Post('/add')
     async postOne(@Body() body, @UploadedFile() file, @Req() req){
-        validate(req.user.username)
+        console.log(req);
+        
         const place = new CreatePlace()
         if(!file) file = {}
         place.place_image = file.path
@@ -71,13 +62,11 @@ export class PlaceController {
     @ApiOperation({description: "This endpoint to deleting place from shop"})
     @Delete('/delete/:id')
     async deleteOne(@Param('id') id: string, @Req() req){
-        validate(req.user.username)
+        
         return this.placeService.deletePlace(+id)
     }
 
-    @ApiBearerAuth()
-    @ApiUnauthorizedResponse()
-    @UseGuards(JwtAuthGuard)
+
     @ApiOperation({description: "This endpoint to editing place in shop"})
     @ApiConsumes('multipart/form-data')
     @ApiBody({
@@ -98,7 +87,7 @@ export class PlaceController {
     @UsePipes(new ValidationPipe())
     @Put('/put/:id')
     async putOne(@Param('id') id: string, @Body() body, @UploadedFile() file, @Req() req){
-        validate(req.user.username)
+        
         const place = new UpdatePlace()
         if(!file) file = {}
         place.place_image = file.path
